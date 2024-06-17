30dbf6a25ef3098fd6f8378e75ba3b4110bfbe04
nguyentranlht <88877923+nguyentranlht@users.noreply.github.com>
2024-06-17 13:39:41 +0700

    role

--- 2180607813_TranThanhNguyen/src/main/java/com/example/__TranThanhNguyen/controller/CustomErrorController.java
+++ 2180607813_TranThanhNguyen/src/main/java/com/example/__TranThanhNguyen/controller/CustomErrorController.java
@@ -15,21 +15,9 @@ import java.util.Optional;
 public class CustomErrorController implements ErrorController {
     @GetMapping
     public String handleError(HttpServletRequest request) {
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-
-        if (status != null) {
-            int statusCode = Integer.parseInt(status.toString());
-
-            if (statusCode == HttpStatus.NOT_FOUND.value()) {
-                return "error/404";
-            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-                return "error/500";
-            }
-        }
-        return "error/generic";
-    }
-
-    public String getErrorPath() {
-        return "/error";
+        return Optional.ofNullable(request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE))
+                .filter(status -> Integer.parseInt(status.toString()) == 404)
+                .map(status -> "error/404")
+                .orElse(null);
     }
 }
